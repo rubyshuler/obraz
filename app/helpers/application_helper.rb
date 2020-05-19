4872020ad735f369281a4cae5ab8606b2bcd9ed9
Shuler <stasia.shuler@gmail.com>
2020-05-19 17:49:46 +0300

    OZ-13 price visualization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def object_price(object)
+    prices = object.items.map {|object| object.price }
+    price = prices.inject(0, :+){|sum,x| sum + x }
+    number_with_delimiter(price, delimiter: " ")
+  end
 end
--- app/helpers/looks_helper.rb
+++ app/helpers/looks_helper.rb
@@ -1,6 +1,7 @@
 module LooksHelper
   def look_price(look)
     prices = look.items.map {|item| item.price }
-    prices.inject(0, :+){|sum,x| sum + x }
+    price = prices.inject(0, :+){|sum,x| sum + x }
+    number_with_delimiter(price, delimiter: " ")
   end
 end
