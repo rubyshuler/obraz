c613ce31832a04b114122d06b0a4699e354f5ebc
Shuler <stasia.shuler@gmail.com>
2020-05-26 17:10:30 +0300

    OZ-17 fixed models

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -5,8 +5,8 @@ class Designer < ApplicationRecord
   has_many :items
 
   has_many :orders
-  # has_many :items_orders
-  # has_many :orders, through: :items_orders
+  has_many :items_orders
+  has_many :orders, through: :items_orders
   accepts_nested_attributes_for :orders
 
   has_one_attached :image
