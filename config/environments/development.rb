6a9c10352f563698990dda36180f3dce4ebc5334
ServerObraz <stasia.shuler@gmail.com>
2020-05-30 06:48:37 +0200

    commit from server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.public_file_server.enabled = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -52,7 +53,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  config.hosts.clear
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
